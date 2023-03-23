c193d9de4839d8e029d7f3695948930ab2d7ac14
MioBestWaifu <Fenix1603@hotmail.com>
2023-03-23 14:46:52 -0300

    True Init

---
+++ Backend/Hestia/Controllers/ConfigController.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hestia.Controllers
+{
+    public partial class ConfigController : Controller
+    {
+        public Label currentIpLabel;
+        public Entry serverPortEntry, DbIpEntry, DbPortEntry, DbUserEntry, DbPasswordEntry,DbNameEntry;
+
+        [RelayCommand]
+        void ConnectToDatabase()
+        {
+
+        }
+    }
+}
---
+++ Backend/Hestia/Controllers/Controller.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hestia.Controllers
+{
+    public abstract partial class Controller : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FortyOfWidth))]
+        [NotifyPropertyChangedFor(nameof(ThirdOfWidth))]
+        [NotifyPropertyChangedFor(nameof(FifthOfWidth))]
+        int width;
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(QuarterOfHeight))]
+        [NotifyPropertyChangedFor(nameof(FifthOfHeight))]
+        int height;
+
+        public int FortyOfWidth => (int)(Width * 0.4);
+        public int FifthOfWidth => (int)(Width * 0.2);
+        public int ThirdOfWidth => Width / 3;
+        public int FifthOfHeight => Height / 5; 
+        public int QuarterOfHeight => Height / 4;
+    }
+}
